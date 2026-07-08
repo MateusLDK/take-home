c369b93252cf18280586d87680f91090b9c5b101
MateusLDK <ladikamateus@gmail.com>
2026-07-08 12:30:35 -0300

    typo fix

--- src/models/exceptions.py
+++ src/models/exceptions.py
@@ -4,14 +4,14 @@ class AccountException(Exception):
     pass
 
 
-class InsufficientFundsError(AccountExceptions):
+class InsufficientFundsError(AccountException):
     "Exception raised when an account has insufficient funds"
 
     def __init__(self, message):
         super().__init__(message)
 
 
-class InexistingAccountError(AccountExceptions):
+class InexistingAccountError(AccountException):
     "Exception raised when an account does not exits"
 
     def __init__(self, message):
